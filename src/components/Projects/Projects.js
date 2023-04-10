c074701b9a7e03ead37be17274dcad00febbec30
patilpavan786 <pavan.p.b.pavan@gmail.com>
2023-04-11 00:22:50 +0530

    portfolio change

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import gym from "../../Assets/Projects/gym.png";
+import TWITTER from "../../Assets/Projects/tweeter.png";
+import Login from "../../Assets/Projects/loginregi.png";
+import SALOON from "../../Assets/Projects/saloon.png";
+import word from "../../Assets/Projects/word.png";
+import CALENDER from "../../Assets/Projects/calender.png";
 
 function Projects() {
   return (
@@ -23,68 +23,67 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={SALOON}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="SALOON WEBSITE"
+              description="its a Hair Saloon website , here user can login & register himself , and he can purchase different packages which are available , user can connect to the owner with email facility"
+              ghLink="https://github.com/patilpavan786/SaloonWebsite.git"
+              demoLink="https://saloon-website.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={CALENDER}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="CALENDER APP"
+              description="Created a calendar app that enables users to add hourly, monthly, and yearly events and tag other registered app users."
+              ghLink="https://github.com/patilpavan786/project4-calenderapp.git"
               demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={Login}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="LOGIN/REGISTER - PORTAL"
+              description="Users can register and login with the same credentials on this portal, and their information will be stored in local storage. Multiple users can register on the portal."
+              ghLink="https://github.com/patilpavan786/login-registerportal.git"
+              demoLink="https://login-registerportal.vercel.app/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={gym}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="GYM WEBSITE"
+              description="This gym website built with React, Recoil, and React Router Dom allows users to register, login, and purchase subscriptions."
+              ghLink="https://github.com/patilpavan786/Project2-newnew.git"
+              demoLink="https://project2-newnew.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={word}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Word Doc Clone"
+              description="Created a Google Docs clone that includes features for text formatting, image insertion, and PDF file saving with custom naming"
+              ghLink="https://github.com/patilpavan786/worddoc/tree/master/worldfile"
+              //  demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" 
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={TWITTER}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="TWITTER CLONE"
+              description="I developed a Twitter clone UI that replicates the platform's core features, including tweet and comment creation and liking."
+              ghLink="https://github.com/patilpavan786/twitter-clone.git"
+               demoLink="https://twitter-clone-seven-phi.vercel.app/"      
             />
           </Col>
         </Row>
